9886b00501e8b0f88d70483367116de665b9751e
Ervin Colo <79028899+ervincolo@users.noreply.github.com>
2025-06-26 23:47:20 +0200

    changes

--- myApp.js
+++ myApp.js
@@ -66,10 +66,13 @@ const findOneByFood = (food, done) => {
   })
 };
 
-
-
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err,data) => {
+    if(err) {
+      return done(err);
+    }
+    done(null,data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
